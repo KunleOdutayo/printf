b6caffa45ce1c6325d933135f8af3bd82c19f8b4
amin ahmed <aminahmed24@gmail.com>
2023-11-15 01:09:38 +0200

    break _printf into smaller functions and follow betty style

--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN__PRINTF
 #define MAIN__PRINTF
 
+#include <stdarg.h>
 
 
 int _printf(const char *format, ...);
+int parsef(const char *format, va_list args);
+int printstr(const char *str);
+int printint(const int num);
 
 
 
--- printf.c
+++ printf.c
@@ -3,75 +3,133 @@
 #include <unistd.h>
 #include "main.h"
 
-int _printf(const char *format, ...){
-    int n = 0;
-    va_list args;
-    va_start(args, format);
+/**
+* printint - prints the given int num
+* @num: the number to print
+* Return: the number of printed characters
+*/
 
-    while (*format != '\0') {
-        if (*format != '%') {
-            write(1, format, 1);
-            n++;
-            format++;
-            continue;
-        }
-        format++;
-        switch (*format) {
-            case '%': {
-                write(1, format, 1);
-                n++;
-                break;
-            }
-            case 'c': {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                n++;
-                break;
-            }
-            case 's': {
-                char* s = va_arg(args, char*) ;
-                while (*s) {
-                    write(1, s++, 1);
-                    n++;
-                }
-                break;
-            }
-            case 'd':
-            case 'i': {
-                int num = va_arg(args, int);
-                char buffer[20];
-                int i = 0;
-                if (num < 0) {
-                    char sign = '-';
-                    write(1, &sign, 1);
-                    n++;
-                    num = -num;
-                }
-                if (num == 0) {
-                    buffer[i++] = '0';
-                }
-                while (num > 0) {
-                    buffer[i++] = '0' + (num % 10);
-                    num /= 10;
-                }
-                while (i--) {
-                    write(1, &buffer[i], 1);
-                    n++;
-                }
-                break;
-            }
+int printint(int num)
+{
+	int n = 0;
+	int i = 0;
+	char sign;
+	char buffer[20];
 
-            default: {
-                write(1, format, 1);
-                n++;
-                break;
-            }
-        }
-        format++;
-    }
+	if (num < 0)
+	{
+		sign = '-';
+		write(1, &sign, 1);
+		n++;
+		num = -num;
+	}
+	if (num == 0)
+	{
+		buffer[i++] = '0';
+	}
+	while (num > 0)
+	{
+		buffer[i++] = '0' + (num % 10);
+		num /= 10;
+	}
+	while (i--)
+	{
+		write(1, &buffer[i], 1);
+		n++;
+	}
 
-    va_end(args);
+	return (n);
+}
 
+/**
+* printstr - prints the given string
+* @str: the string to print
+* Return: the number of printed characters
+*/
+int printstr(const char *str)
+{
+	int n = 0;
 
-    return n;
+	while (*str)
+	{
+		write(1, str++, 1);
+		n++;
+	}
+
+	return (n);
+}
+
+/**
+* parsef - parses the format directives
+* @format: the format string
+* @args: the arguments
+* Return: the number of printed characters
+*/
+int parsef(const char *format, va_list args)
+{
+	int n = 0;
+	char c;
+	char *s;
+	int num;
+
+	switch (*format)
+	{
+		case '%':
+			write(1, format, 1);
+			n++;
+		break;
+		case 'c':
+			c = va_arg(args, int);
+			write(1, &c, 1);
+			n++;
+		break;
+		case 's':
+			s = va_arg(args, char*);
+			n += printstr(s);
+		break;
+		case 'd':
+		case 'i':
+			num = va_arg(args, int);
+			n += printint(num);
+		break;
+		default:
+			write(1, format, 1);
+			n++;
+		break;
+	}
+
+	return (n);
+}
+
+/**
+* _printf - print a formated string
+* @format: the format string
+* @...: the variadic arguments
+* Return: the number of printed characters
+*/
+int _printf(const char *format, ...)
+{
+	int n = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			n++;
+			format++;
+			continue;
+		}
+		format++;
+		n += parsef(format, args);
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (n);
 }
