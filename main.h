ec63626441e99c847b256568f22742bc294fb06e
Ayokunle Odutayo <kunlay.odutayo@gmail.com>
2023-11-17 15:18:35 -0800

    smaller functions

--- main.h
+++ main.h
@@ -5,10 +5,12 @@
 
 
 int _printf(const char *format, ...);
-int parsef(const char *format, va_list args);
-int printstr(const char *str);
-int printint(const int num);
-
+int parsef(const char *format, va_list args, int *n);
+int printstr(const char *str, int *n);
+int printint(const int num, int *n);
+int printbin(unsigned int num, int *n);
+int printuint(unsigned int num, int *n);
+int printoct(unsigned int num, int *n);
 
 
 #endif
